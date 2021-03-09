a4fb8acaa84924e44230b67dbafdb27eb81d9c02
Issa Al-aiash <izalayyash152@eng.just.edu.jo>
2021-03-09 03:01:28 +0200

    pushing the project work, not done yet

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ CookieStand.prototype.data=function(){
       tableHead.innerText=this.location;
       tableRow.appendChild(tableHead);
     }
-    tableData.innerText=Math.floor(Math.random()*(this.maxSales-this.minSales)+this.minSales);
+    tableData.innerText=Math.floor((Math.random()*(this.maxSales-this.minSales-1)+this.minSales)*this.averageSales);
     houlrySum+=parseInt(tableData.innerText);
     if(i===2){
       locationSum=houlrySum;
@@ -62,76 +62,14 @@ tableRow.appendChild(tableHead);
 tableData.innerText=locationSum;
 tableRow.appendChild(tableData);
 tableData=document.createElement('td');
-tableHead.innerText='issa';
-let Seattle= new CookieStand(10,100,15,'Seattle');
+tableHead.innerText='Total for the hour';
+let Seattle= new CookieStand(23,65,6.3,'Seattle');
 Seattle.data();
-let Tokyo=new CookieStand(25,50,10,'Tokyo');
+let Tokyo=new CookieStand(3,24,1.2,'Tokyo');
 Tokyo.data();
-let Dubai=new CookieStand(25,50,10,'Dubai');
+let Dubai=new CookieStand(11,38,3.7,'Dubai');
 Dubai.data();
-let Paris=new CookieStand(25,50,10,'Paris');
+let Paris=new CookieStand(20,38,2.3,'Paris');
 Paris.data();
-let Lima=new CookieStand(25,50,10,'Lima');
+let Lima=new CookieStand(2,16,4.6,'Lima');
 Lima.data();
-// 'use strict';
-// let hours=['','6 am','7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm','8 pm'];
-// let houlrySum=0;
-// let locationSum=0;
-// let main=document.getElementById('sales');
-// let tableHead=document.createElement('th');
-// let tableRow=document.createElement('tr');
-// let table=document.createElement('table');
-// let tableData=document.createElement('td');
-// let x=[];
-// main.appendChild(table);
-// table.appendChild(tableRow);
-// tableRow.appendChild(tableHead);
-// for(let i=0;i<=hours.length;i++){
-//   if(i<hours.length){
-//     tableHead.innerText=(hours[i]);
-//     tableHead=document.createElement('th');
-//     tableRow.appendChild(tableHead);
-//   }else if(i===hours.length){
-//     tableHead.innerText=('Total sales for the day' );
-//   }
-// }
-// function CookieStand(minSales,maxSales,averageSales,location){
-//   this.minSales=minSales;
-//   this.maxSales=maxSales;
-//   this.averageSales=averageSales;
-//   this.location=location;
-// }
-// CookieStand.prototype.data=function(){
-//   tableRow=document.createElement('tr');
-//   table.appendChild(tableRow);
-//   tableHead=document.createElement('th');
-//   houlrySum=0;
-//   for(let i=0;i<hours.length;i++){
-//     if(i===0){
-//       tableHead.innerText=this.location;
-//       tableRow.appendChild(tableHead);
-//     }
-//     if(i===hours.length-1){
-//       tableData.innerText=houlrySum;
-//       tableRow.appendChild(tableData);
-//       tableData=document.createElement('td');
-//       break;
-//     }
-//     tableData.innerText=Math.floor(Math.random()*(this.maxSales-this.minSales)+this.minSales);
-//     houlrySum+=parseInt(tableData.innerText);
-//     tableRow.appendChild(tableData);
-//     tableData=document.createElement('td');
-//   }
-// };
-// let Seattle= new CookieStand(10,100,15,'Seattle');
-// Seattle.data();
-// let Tokyo=new CookieStand(25,50,10,'Tokyo');
-// Tokyo.data();
-// let Dubai=new CookieStand(25,50,10,'Dubai');
-// Dubai.data();
-// let Paris=new CookieStand(25,50,10,'Paris');
-// Paris.data();
-// let Lima=new CookieStand(25,50,10,'Lima');
-// Lima.data();
-// let Total=new CookieStand(0,0,0,'sum');
-// Total.data();
